d2c4fd0462c9d2614b97e4ffed428d14c68e764f
Jopik1 <47256803+Jopik1@users.noreply.github.com>
2019-02-27 15:10:03 +0200

    Add files via upload

--- deploy.sh
+++ deploy.sh
@@ -13,4 +13,18 @@ cd prj
 git clone https://github.com/afrmtbl/blogspot-comment-backup
 chown -R fetch:fetch *
 cd blogspot-comment-backup/src
-su - fetch -c "cd ~/prj/blogspot-comment-backup/src; nohup python3.7 worker.py &"
+
+cat > runloop.sh <<EOL
+#!/bin/bash
+cd ~/prj/blogspot-comment-backup/src
+while true
+do
+	python3.7 worker.py
+	sleep 1
+done
+EOL
+
+su - fetch -c "nohup ~/prj/blogspot-comment-backup/src/runloop.sh &"
+
+nohup tail -f ~fetch/prj/blogspot-comment-backup/src/nohup.out | >/dev/tty1 2>&1 &
+
